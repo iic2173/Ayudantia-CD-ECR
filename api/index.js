140a920fa62629fa18e1832b19aa08a108f44310
Emilia Ureta Pulido <eureta1@uc.cl>
2024-04-28 16:43:29 -0400

    Primera etapa

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// GET endpoint
+app.get('/', (req, res) => {
+  res.send('Hello World! This is a GET request.');
+});
+
+// POST endpoint
+app.post('/', (req, res) => {
+  const requestBody = req.body;
+  // Do something with the request body, for example, log it
+  console.log('Received POST request with body:', requestBody);
+  res.send('POST request received successfully.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
