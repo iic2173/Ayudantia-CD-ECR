9a1d9b04babf1af30f38019a29b23732942f2fb0
Emilia Ureta Pulido <eureta1@uc.cl>
2024-04-30 19:18:06 -0400

    changed js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // GET endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World! This is a GET request');
+  res.send('Hello World! This is a GET request parte 2');
 });
 
 // POST endpoint
